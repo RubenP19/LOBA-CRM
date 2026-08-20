4d5c74682b7bb35f6043fa8d94ca8d25a4082704
RubenP19 <ruben.peres19@gmail.com>
2026-08-20 17:03:57 +0100

    Update 03_sentinela.js

--- Fundamentos de Programação/Tarefa de Consolidação 2/03_sentinela.js
+++ Fundamentos de Programação/Tarefa de Consolidação 2/03_sentinela.js
@@ -1,16 +1,31 @@
+const prompt = require('prompt-sync')({sigint: true});
+
 //Pedir orçamento das leads (à vez) até escrever "Fim". Nesse momento calcular total e média
 
-let orc = []
+let orc = [];
+
+while (true) {
+
+    let entrada = prompt("Orçamento " + (orc.length + 1) + " (ou 'Fim'): ");
+
+    if (entrada.toLowerCase() === "fim") {
+        break;
+    }
+
+    if (isNaN(valor)) {
+        console.log("Erro: introduza apenas um número ou 'Fim'.");
+        continue;}
+// Converter para número
+    let valor = Number(entrada);
+    
+
 let total = 0;
 
-for orc (i=0; i < orc.length; i++){
-total = total + orc[i];
+for (let i = 0; i < orc.length; i++) {
+    total = total + orc[i];
 }
 
+let media = total / orc.length;
 
-const media = total / orcamentos.length;
-
-
-//Total Média e Orçamento
-// console.log("Total dos orçamentos: " + total);
-// console.log("Média dos orçamentos: " + media);
+console.log("Total dos orçamentos: " + total);
+console.log("Média dos orçamentos: " + media);
