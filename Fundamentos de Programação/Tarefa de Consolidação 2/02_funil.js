f0ef9a4da2e36c45b50a34b6a4bf8a919210fd3b
RubenP19 <ruben.peres19@gmail.com>
2026-08-20 14:55:40 +0100

    Update 02_funil.js

--- Fundamentos de Programação/Tarefa de Consolidação 2/02_funil.js
+++ Fundamentos de Programação/Tarefa de Consolidação 2/02_funil.js
@@ -1,18 +1,17 @@
+const prompt = require('prompt-sync')({sigint: true});
 const funil = prompt("Qual a fase do seu lead? (Novo, Contactado, Proposta, Negociação, Ganho) ");
 
-f
-
-
-function proximaAcao(funil) {
-  if (funil === "Novo") {
+function proximaAcao(funil) { 
+    const funil2 = funil.toLowerCase()
+  if (funil2 === "novo") {
     return "Contactar";
-  } else if (funil === "Contactado") {
+  } else if (funil2 === "contactado") {
     return "Fazer proposta";
-  } else if (funil === "Proposta") {
+  } else if (funil2 === "proposta") {
     return "Negociar";
-  } else if (funil === "Negociação") {
+  } else if (funil2 === "negociação") {
     return "Fechar Caso";
-  } else if (funil === "Ganho") {
+  } else if (funil2 === "ganho") {
     return "Ciclo Fechado";
   } else {
     return "Fase inválida";
