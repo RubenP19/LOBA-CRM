902ee438a2d2ec360928450acfaa96b1d244fa65
RubenP19 <ruben.peres19@gmail.com>
2026-08-20 16:44:17 +0100

    Update 03_sentinela.js

--- Fundamentos de Programação/Tarefa de Consolidação 2/03_sentinela.js
+++ Fundamentos de Programação/Tarefa de Consolidação 2/03_sentinela.js
@@ -1,1 +1,16 @@
 //Pedir orçamento das leads (à vez) até escrever "Fim". Nesse momento calcular total e média
+
+let orc = []
+let total = 0;
+
+for orc (i=0; i < orc.length; i++){
+total = total + orc[i];
+}
+
+
+const media = total / orcamentos.length;
+
+
+//Total Média e Orçamento
+// console.log("Total dos orçamentos: " + total);
+// console.log("Média dos orçamentos: " + media);
